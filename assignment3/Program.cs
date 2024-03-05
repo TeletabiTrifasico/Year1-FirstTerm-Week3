ca0f71e94846ef1c29f754e3b2f43a5055e2c9b6
Hugo Jiménez Barrasa <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-03-05 16:54:42 +0100

    Add files via upload

---
+++ assignment3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml;
+
+namespace assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number: ");
+            string firstInput = Console.ReadLine();
+            double firstNumber = Convert.ToDouble(firstInput);
+            Console.WriteLine("Enter second number: ");
+            string secondInput = Console.ReadLine();
+            double secondNumber = Convert.ToDouble(secondInput);
+            Console.WriteLine("Enter third number: ");
+            string thirdInput = Console.ReadLine();
+            double thirdNumber = Convert.ToDouble(thirdInput);
+
+            if (firstNumber > thirdNumber && secondNumber > thirdNumber)
+            {
+                Console.WriteLine("The third number is the smallest of the three");
+            }
+            else
+            {
+                Console.WriteLine("The third number is not the smallest of the three");
+            }
+        }
+    }
+}
