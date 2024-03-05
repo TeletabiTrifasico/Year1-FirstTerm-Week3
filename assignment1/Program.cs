bf76f708ae771b5476f399b3b59ef7da6a7d774c
Hugo Jiménez Barrasa <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-03-05 16:53:05 +0100

    Add files via upload

---
+++ assignment1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number: ");
+            string firstInput = Console.ReadLine();
+            double firstNumber = Convert.ToDouble(firstInput);
+            Console.WriteLine("Enter second number: ");
+            string secondInput = Console.ReadLine();
+            double secondNumber = Convert.ToDouble(secondInput);
+
+            Console.Write("highest value is: ");
+            if (firstNumber > secondNumber)
+            {
+                Console.WriteLine(firstNumber);
+            }
+            else 
+            {
+                Console.WriteLine(secondNumber);
+            }
+
+            Console.Write("lowest value is: ");
+            if (firstNumber < secondNumber)
+            {
+                Console.WriteLine(firstNumber);
+            }
+            else
+            {
+                Console.WriteLine(secondNumber);
+            }
+        }
+    }
+}
