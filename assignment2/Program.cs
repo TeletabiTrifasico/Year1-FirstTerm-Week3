11c7bbda8b40aa6cbcbc5481c0d2835e6d8bb809
Hugo Jiménez Barrasa <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-03-05 16:53:36 +0100

    Add files via upload

---
+++ assignment2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number (1..4): ");
+            string input = Console.ReadLine();
+            int usersNumber = Convert.ToInt32(input);
+         
+            switch (usersNumber)
+            {
+                case 1:
+                    Console.WriteLine("clubs");
+                    break;
+                case 2:
+                    Console.WriteLine("diamonds");
+                    break;
+                case 3:
+                    Console.WriteLine("hearts");
+                    break;
+                case 4:
+                    Console.WriteLine("spades");
+                    break;
+                default:
+                    Console.WriteLine("Incorrect number! ");
+                    break; 
+            }
+        }
+    }
+}
